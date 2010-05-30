b3560ac00fbde90ab1822195c415434594be3b20
markus.santaniemi <markus.santaniemi@9bdd7f2c-2df9-11df-8971-ebb891de2ff2>
2010-05-30 11:40:14 +0000

    Source tree additions: add ui related classes...
    git-svn-id: http://rexprojectspace.googlecode.com/svn/trunk@49 9bdd7f2c-2df9-11df-8971-ebb891de2ff2

--- src/swsourcetree.py
+++ src/swsourcetree.py
@@ -13,6 +13,77 @@ clr.AddReference('OpenSim.Region.ScriptEngine.Shared')
 from OpenSim.Region.ScriptEngine.Shared import LSL_Types
 clr.AddReference('OpenSim.Framework')
 
+V3 = asm.OpenSim.Region.ScriptEngine.Shared.LSL_Types.Vector3
+
+#UI stuff
+class Tree:
+
+    def __init__(self,vName):
+        """ "Base" of the tree """
+        rexObjects = self.scene.Modules["RexObjectsModule"]
+        self.UUID = OpenMetaverse.UUID("ac1e69a2-b56a-4cd6-bf95-360aa1dd27c4") #root of tree...
+        self.rop = rexObjects.GetObject(self.UUID)
+    
+        print "mesh id___"
+        print self.rop.RexMeshUUID
+        
+        self.tiles = [] #from bottom to up
+        #tiles.append(TreeTile())
+        
+        self.treeTop = None
+        
+        self.branches = []
+        
+    
+    def addNewBranch(self,vBranchName,vParentName=None):
+        """None means to add branch to main tree, otherwise add to
+           other branch """           
+        if vParentName == None:
+            nbr = len(self.tiles)
+            tile = self.tiles[nbr]
+            currentPlace = tile.currentIndex
+            #create branch and locate it
+            branch = Branch(vBranchName,None,currentPlace,V3(1,1,1),V3(0,0,0))
+            if tile.currentIndex < len(tile.locations):
+                tile.currentIndex += 1
+            else:
+                self.tiles.append(TreeTile)
+            
+            self.branches.append(branch)
+            
+class TreeTile:
+    def __init__(self,vPos):
+        locations = [] #insert branch locations to here… (with normal scale)
+        currentIndex = 0 #increment on add...
+        self.pos = vPos
+        
+        rexObjects = self.scene.Modules["RexObjectsModule"]        
+        
+        self.sog, self.rop = rexprojectspaceutils.load_mesh(self.scene,"treetile.mesh","treetile.material","tile…",OpenMetaverse.Quaternion(0,0,0,1),self.pos)
+        #rexObjects.GetObject(self.UUID)
+    
+        print "mesh id___"
+        print self.rop.RexMeshUUID
+                      
+        self.branches = []
+        
+    
+class Branch:
+    def __init__(self,vName,vParentName,vPos,vScale,vRot):
+    
+        """ "Branch" of the tree """
+        rexObjects = self.scene.Modules["RexObjectsModule"]
+        self.UUID = OpenMetaverse.UUID("ac1e69a2-b56a-4cd6-bf95-360aa1dd27c4") #branch of tree...
+        self.rop = rexObjects.GetObject(self.UUID)
+        self.parent = vParent
+        
+        print "mesh id___"
+        print self.rop.RexMeshUUID
+
+
+    pass    
+
+#data
 class SWSourceTree:
     
     def __init__(self, vScene, vProjectName, vBranchNames):
@@ -20,14 +91,14 @@ class SWSourceTree:
         self.scene = vScene
         self.projectName = vProjectName
         self.branches = []
-        self.UUID = OpenMetaverse.UUID("ac1e69a2-b56a-4cd6-bf95-360aa1dd27c4")
-        
+               
         self.bCurrentBuildFailed = False
         
-        rexObjects = self.scene.Modules["RexObjectsModule"]
-        self.rop = rexObjects.GetObject(self.UUID)
-        print "mesh id___"
-        print self.rop.RexMeshUUID
+        self.tree = Tree()
+                
+        #start from bottom
+        for branch in vBranchNames:
+            self.tree.branches.append(self.addNewBranch(branch))
         
         
     def setBuildSuccesfull(self):
@@ -46,6 +117,11 @@ class SWSourceTree:
             pass
         
         self.bCurrentBuildFailed = True
+        
+    def addNewBranch(self,vBranchName,vParent=None):
+        """None means to add branch to main tree, otherwise add to
+           other branch """           
+        pass        
         
 class SWSourceTreeBranch:
     
