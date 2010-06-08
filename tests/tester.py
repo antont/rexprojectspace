0b77084a69654810d996544cdc49a4202d5dc0fc
markus.santaniemi <markus.santaniemi@9bdd7f2c-2df9-11df-8971-ebb891de2ff2>
2010-06-08 11:12:21 +0000

    more test code related to commits...
    git-svn-id: http://rexprojectspace.googlecode.com/svn/trunk@55 9bdd7f2c-2df9-11df-8971-ebb891de2ff2

--- tests/tester.py
+++ tests/tester.py
@@ -3,6 +3,45 @@ from issuetracker import *
 from buildbot import *
 
 
+def resolveFolder(vCommit):
+	#print vCommit
+	#get mod,add,remove
+	files,mod,removed,added = [],[],[],[]
+	try:
+		mod = vCommit["modified"]
+	except:
+		pass
+	try:
+		removed = vCommit["Removed"]
+	except:
+		pass
+	try:
+		added = vCommit["added"]
+	except:
+		pass
+	
+	for m in mod:
+		files.append(m["filename"])	
+	
+	for a in added:
+		files.append(a)	
+	
+	for r in removed:
+		files.append(r)
+				
+
+	#files.sort()		
+	
+	modifiedfiles = list(set(files))
+	
+	filesandcounts = {}
+	
+	for file in modifiedfiles:
+		filesandcounts[file] = files.count(file)	
+		
+	print filesandcounts
+
+
 #from swdeveloper import *
 
 v = VersionControlSystem("naali")
@@ -18,19 +57,23 @@ v = VersionControlSystem("naali")
 
 ### get lates commit and print modified file list
 
-latestCommit = v.getLatestCommitForBranch()
+#latestCommit = v.getLatestCommitForBranch()
 
-commitId = latestCommit["id"]
+#commitId = latestCommit["id"]
 
-commitInfo = v.getCommitInformation(commitId)
+#commitInfo = v.getCommitInformation(commitId)
+
+commitInfo = v.getCommitInformation("8a0f0cd5b1603dca2e2a0d6f5238a6e6909bb683")
 
 
 commit = commitInfo["commit"]
 
 mod = commit["modified"]
 
-for m in mod:
-	print m["filename"]
+#for m in mod:
+#	print m["filename"]
+
+resolveFolder(commit)
 
 
 """
